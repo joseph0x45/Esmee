a498ff11fc18e5be22cc9bb1c05c955e034f8872
TheWisePigeon <zozozozeph@gmail.com>
2022-09-15 22:14:28 +0000

    feat: added climb up and tree cmd

--- esmee/exposedCli.go
+++ esmee/exposedCli.go
@@ -8,15 +8,26 @@ import (
 )
 
 func exposedCli() {
-
+	var root string = ">"
 	for {
-		fmt.Print(">")
+		fmt.Print(root)
 		reader := bufio.NewReader(os.Stdin)
 		cmdReader, _ := reader.ReadString('\n')
 		result := interpreter(strings.TrimSpace(cmdReader))
-		fmt.Println(result)
-		if result=="Bye" {
+		if result == "Bye" {
 			break
+		}
+		switch {
+		case strings.Contains(result, "climb"):
+			action := strings.Split(result, " ")[0]
+			treeName := strings.Split(result, " ")[1]
+			switch {
+			case action=="climb":
+				root += fmt.Sprintf("%v#", treeName)
+				fmt.Printf("You are now in tree %v\n", treeName)
+			case action=="climbdown":
+				root = strings.Replace(root, fmt.Sprintf("%v#", treeName), "", 1)
+			}
 		}
 	}
 }
--- esmee/interpreter.go
+++ esmee/interpreter.go
@@ -13,43 +13,56 @@ func pathResolver(path string) (resolvedPath string) {
 }
 
 func interpreter(command string) (result string) {
-	switch  {
+	switch {
 
-		//ANCHOR Help and exit
-		case command=="Help":
-			result = "Welcome to EsmeeDB exposed CLI"
-		case command== "help":
-			result = "Welcome to EsmeeDB exposed CLI"
-		case command== "exit":
-			result = "Bye"
+	//ANCHOR Help and exit
+	case command == "Help":
+		result = "Welcome to EsmeeDB exposed CLI"
+	case command == "help":
+		result = "Welcome to EsmeeDB exposed CLI"
+	case command == "exit":
+		result = "Bye"
 
-		//ANCHOR CLIMB
-		case strings.Contains(command, "climb"):
-			subCommands := strings.Split(command, " ")
-			switch{
-				case len(subCommands)>2 || len(subCommands)<2:
-					result = "Invalid syntax. Climb requires only one argument which is the tree name"
-				case len(subCommands)==2:
-					treeName := subCommands[1]
-					result = treeName
+	//ANCHOR CLIMB
+	case strings.Contains(command, "climb"):
+		subCommands := strings.Split(command, " ")
+		switch {
+		case len(subCommands) > 2 || len(subCommands) < 2:
+			result = "Invalid syntax. Climb requires only one argument which is the tree name"
+		case len(subCommands) == 2:
+			treeName := subCommands[1]
+			switch {
+				case treeName=="down" :
+					if rootTree!="root/" {
+						treeName:= strings.Split(rootTree, "/")
+						result = fmt.Sprintf("climbdown %v", treeName[1])
+						rootTree = "root/"
+						return
+					}
+					rootTree = fmt.Sprintf("%v%v", rootTree, treeName)
+					result = fmt.Sprintf("climb %v", treeName)
+				default:
+					rootTree = fmt.Sprintf("%v%v", rootTree, treeName)
+					result = fmt.Sprintf("climb %v", treeName)
 			}
 
-		
-			//ANCHOR CREATES
-		case strings.Contains(command, "create") :
-			subCommands := strings.Split(command, " ")
-			switch  {
-				case len(subCommands)>3 || len(subCommands)<3 :
-					result = "Invalid syntax"
-				case subCommands[1]=="tree":
-					result = fmt.Sprintf("wants to create a tree named %v", subCommands[2])
-				case subCommands[1]=="branch":
-					result = fmt.Sprintf("wants to create a branch named %v", subCommands[2])
-			}
-		default:
-			fmt.Printf("<%v>", command)
+		}
+
+		//ANCHOR CREATES
+	case strings.Contains(command, "create"):
+		subCommands := strings.Split(command, " ")
+		switch {
+		case len(subCommands) > 3 || len(subCommands) < 3:
+			result = "Invalid syntax"
+		case subCommands[1] == "tree":
+			result = fmt.Sprintf("wants to create a tree named %v", subCommands[2])
+		case subCommands[1] == "branch":
+			result = fmt.Sprintf("wants to create a branch named %v", subCommands[2])
+		}
+	default:
+		fmt.Printf("Unrecognized command <%v>", command)
 	}
 
 	return
-	
+
 }
